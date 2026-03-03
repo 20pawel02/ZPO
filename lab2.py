c7e208f47110e04adaa5d73f87c93e691cb558f4
Pawel Wrobel <115549665+20pawel02@users.noreply.github.com>
2026-03-03 16:28:23 +0100

    Update lab2.py
    to do:
    zadanie spod linku: https://github.com/betacord/ZPO/blob/main/2_creational_patterns.ipynb

--- lab2.py
+++ lab2.py
@@ -1,1 +1,28 @@
+# ================================== BUDOWNICZY =================================
+""" 
+A. Przygotować klasę Pizza, która będzie mogła zawierać różne składniki (ser, salami, pieczarki, cebula itd.).
+Zastosować wzorzec budowniczego, aby umożliwić stopniowe dodawanie składników do pizzy.
+
+B. Rozszerzyć istniejącą implementację budowniczego tak, aby umożliwić budowanie różnych
+wariantów obiektów (np. dla pizzy vege, mięsnej, serowej, itd.).
+
+C. Przygotować klasę Computer, która posiada wiele parametrów przekazywanych w inicjalizatorze.
+Przerobić nastęopnie kod tak, aby zamiast dużego konstruktora użyć wzorca budowniczego
+
+"""
+
+
+
+
+# ================================ METODA WYTWÓRCZA ================================
+"""
+A. Utworzyć interfejs Document i klasy: WordDocument, PDFDocument, a następnie przygotować metodę wytwórczą, 
+która decyduje, jaki dokument utworzyć na podstawie zadanego rozszerzenia pliku.
+
+B. Utworzyć klasę AnimalFactory, która na podstawie podanego parametru (np. "dog", "cat")
+zwraca obiekt odpowiedniej klasy (Dog, Cat).
+ 
+C. Rozbuduj przygotowane implementacje Metody Wytwórczej, tak aby mogły obsługiwać dynamiczne 
+rejestrowanie nowych klas zamiast statycznych instrukcji warunkowych.
+"""
 
