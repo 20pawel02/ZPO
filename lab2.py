a02986f0484bd644997e92075e4a9d9d367e8cfc
20pawel02 <20pawel02@wp.pl>
2026-03-10 17:01:45 +0100

    :)))))

--- lab2.py
+++ lab2.py
@@ -2,6 +2,12 @@
 """
 A. Przygotować klasę Pizza, która będzie mogła zawierać różne składniki (ser, salami, pieczarki, cebula itd.).
 Zastosować wzorzec budowniczego, aby umożliwić stopniowe dodawanie składników do pizzy.
+
+B. Rozszerzyć istniejącą implementację budowniczego tak, aby umożliwić budowanie różnych
+wariantów obiektów (np. dla pizzy vege, mięsnej, serowej, itd.).
+
+C. Przygotować klasę Computer, która posiada wiele parametrów przekazywanych w inicjalizatorze.
+Przerobić nastęopnie kod tak, aby zamiast dużego konstruktora użyć wzorca budowniczego
 """
 
 from abc import ABC, abstractmethod
@@ -12,30 +18,56 @@ class Pizza(ABC):
         self.skladniki = []
 
     def add_skladnik(self, skladniki: str) -> None:
-        self.skladniki.append(skladniki)
+        pass
 
     @abstractmethod
     def show_skladniki(self) -> str:
         pass
 
-class SredniaHawajska(Pizza):
+# Builder
+class SredniaHawajskaDlaKazdego(Pizza):  # sponsorowana przez admirala Hajasia -- Glak Pizza
+
+    def add_skladnik(self, skladnik: str) -> None:
+        self.skladniki.append(skladnik)
+
     def show_skladniki(self) -> str:
         return f"{', '.join(self.skladniki)}"
 
+# Builder
+class PizzaDiavola(Pizza):
+    def add_skladnik(self, skladnik: str) -> None:
+        self.skladniki.append(skladnik)
+    
+    def show_skladniki(self) -> str:
+        return f"{', '.join(self.skladniki)}"
+
+
+
+# C. Przygotować klasę Computer, która posiada wiele parametrów przekazywanych w inicjalizatorze.
+# Przerobić nastęopnie kod tak, aby zamiast dużego konstruktora użyć wzorca budowniczego
+
+class BuilderComputer(ABC):
+    @abstractmethod
+    def build_cpu(self) -> None:
+        pass
+
+    @abstractmethod
+    def build_gpu(self) -> None:
+        pass
+
+
+
+
+
 if __name__ == '__main__':
-    obj = SredniaHawajska()
-    obj.add_skladnik('ananas')
-    obj.add_skladnik('pieczarki')
-    print(obj.show_skladniki())
+    obj = SredniaHawajskaDlaKazdego()
+    pass
 
 
-"""
-B. Rozszerzyć istniejącą implementację budowniczego tak, aby umożliwić budowanie różnych
-wariantów obiektów (np. dla pizzy vege, mięsnej, serowej, itd.).
 
-C. Przygotować klasę Computer, która posiada wiele parametrów przekazywanych w inicjalizatorze.
-Przerobić nastęopnie kod tak, aby zamiast dużego konstruktora użyć wzorca budowniczego
-"""
+
+
+
 
 # ================================ METODA WYTWÓRCZA ================================
 """
@@ -44,7 +76,7 @@ która decyduje, jaki dokument utworzyć na podstawie zadanego rozszerzenia plik
 
 B. Utworzyć klasę AnimalFactory, która na podstawie podanego parametru (np. "dog", "cat")
 zwraca obiekt odpowiedniej klasy (Dog, Cat).
- 
+
 C. Rozbuduj przygotowane implementacje Metody Wytwórczej, tak aby mogły obsługiwać dynamiczne 
 rejestrowanie nowych klas zamiast statycznych instrukcji warunkowych.
 """
