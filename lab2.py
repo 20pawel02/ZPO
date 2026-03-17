f3668dd540d831809d92206816dc9fea60a0b2c3
20pawel02 <20pawel02@wp.pl>
2026-03-17 16:47:07 +0100

    Update lab2.py

--- lab2.py
+++ lab2.py
@@ -45,6 +45,7 @@ class PizzaDiavola(Pizza):
 
 # C. Przygotować klasę Computer, która posiada wiele parametrów przekazywanych w inicjalizatorze.
 # Przerobić nastęopnie kod tak, aby zamiast dużego konstruktora użyć wzorca budowniczego
+from abc import ABC, abstractmethod
 
 class Computer:
     def __init__(self) -> None:
@@ -52,43 +53,133 @@ class Computer:
         self.gpu = ""
         self.psu = ""
         self.ram = ""
-        self.storage = 0
+        self.storage = ""
+
+    def __str__(self) -> str:
+        return f"Computer [CPU: {self.cpu}, GPU: {self.gpu}, PSU: {self.psu}, RAM: {self.ram}, Storage: {self.storage}]"
 
 class BuilderComputer(ABC):
+    def __init__(self):
+        self._computer = Computer() 
+    
     @abstractmethod
-    def build_cpu(self) -> None:
-        pass
+    def build_cpu(self) -> None: pass
 
     @abstractmethod
-    def build_gpu(self) -> None:
-        pass
+    def build_gpu(self) -> None: pass
 
     @abstractmethod
-    def build_psu(self) -> None:
-        pass
+    def build_psu(self) -> None: pass
 
     @abstractmethod
-    def build_ram(self) -> None:
-        pass
+    def build_ram(self) -> None: pass
 
     @abstractmethod
-    def build_storage(self) -> None:
-        pass
+    def build_storage(self) -> None: pass
 
     def get_computer(self) -> Computer:
-        return self.computer
-
-class 
+        return self._computer
 
 
+class MyComputer_BuilderComputer(BuilderComputer):
+    def __init__(self):
+        self.reset()
+
+    def reset(self) -> None:
+        self._computer = Computer()
+
+    @property
+    def computer(self) -> Computer:
+        build_computer = self._computer
+        self.reset()
+        return build_computer
+
+    def build_cpu(self) -> None:
+        self._computer.cpu = "Ryzen 7 5800X"
+
+    def build_gpu(self) -> None:
+        self._computer.gpu = "Radeon 6700"
+
+    def build_psu(self) -> None:
+        self._computer.psu = "700W"
+
+    def build_ram(self) -> None:
+        self._computer.ram = "64GB"
+
+    def build_storage(self) -> None:
+        self._computer.storage = "1TB hdd, 500GB NVMe"
+
+class OfficeComputer_BuilderComputer(BuilderComputer):
+    def __init__(self):
+        self.reset()
+
+    def reset(self) -> None:
+        self._computer = Computer()
+
+    @property
+    def computer(self) -> Computer:
+        build_computer = self._computer
+        self.reset()
+        return build_computer
+    
+    def build_cpu(self) -> None:
+        self._computer.cpu = "Intel Core i3"
+
+    def build_gpu(self) -> None:
+        self._computer.gpu = "Zintegrowana karta graficzna"
+
+    # DODANO BRAKUJĄCĄ METODĘ
+    def build_psu(self) -> None:
+        self._computer.psu = "300W Silent"
+
+    def build_ram(self) -> None:
+        self._computer.ram = "8GB DDR4"
+
+    def build_storage(self) -> None:
+        self._computer.storage = "512GB SSD"
+
+
+class Director:
+    _builder: BuilderComputer
+    _requirements: dict
+
+    def set_requirements(self, requirements: dict) -> None:
+        self._requirements = requirements
+        self._set_builder()
+
+    def build_computer(self) -> Computer:
+        self._builder.build_cpu()
+        self._builder.build_gpu()
+        self._builder.build_psu()
+        self._builder.build_ram()
+        self._builder.build_storage()
+
+        return self._builder.computer
+
+    def _set_builder(self) -> None:
+        if self._requirements.get("gpu_intensive", False) is True:
+            self._builder = MyComputer_BuilderComputer()
+        else:
+            self._builder = OfficeComputer_BuilderComputer()
 
 
 if __name__ == '__main__':
-    obj = SredniaHawajskaDlaKazdego()
-    pass
+    heavy_workload = {"gpu_intensive": True, "budget": 15000}
+    light_workload = {"gpu_intensive": False, "budget": 3000}
 
+    director = Director()
 
+    director.set_requirements(heavy_workload)
+    gaming_pc = director.build_computer()
+    print("Gaming PC:")
+    print(gaming_pc)
 
+    print("\n")
+
+    director.set_requirements(light_workload)
+    office_pc = director.build_computer()
+    print("Office PC:")
+    print(office_pc)
 
 
 
@@ -104,3 +195,15 @@ zwraca obiekt odpowiedniej klasy (Dog, Cat).
 C. Rozbuduj przygotowane implementacje Metody Wytwórczej, tak aby mogły obsługiwać dynamiczne 
 rejestrowanie nowych klas zamiast statycznych instrukcji warunkowych.
 """
+
+
+
+class WordDocument(ABC):
+    
+    pass
+
+
+
+
+class PDFDocument:
+    pass
